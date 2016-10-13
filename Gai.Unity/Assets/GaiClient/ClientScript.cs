3652a1a8338006c5ca84fac319fc8e151b497fb4
ColacX <DanielZheng1989@gmail.com>
2016-10-13 17:52:22 +0200

    added working error free first scene using the rpg framework

---
+++ Gai.Unity/Assets/GaiClient/ClientScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClientScript : MonoBehaviour {
+
+	public GameObject playerPrefab;
+
+	// Use this for initialization
+	void Start () {
+		//var playerPrefab = (GameObject)Resources.Load("/Assets/CreativeSpore/RpgMapEditor/Samples/Prefabs/Player.prefab", typeof(GameObject));
+		Instantiate(playerPrefab, new Vector3(30,-25), Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
