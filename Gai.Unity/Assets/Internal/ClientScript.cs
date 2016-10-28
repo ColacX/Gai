c54122aee7355776ad10342cb0491f2ad434e4fe
ColacX <DanielZheng1989@gmail.com>
2016-10-28 20:46:45 +0200

    fixed bugs loading map

--- Gai.Unity/Assets/Internal/ClientScript.cs
+++ Gai.Unity/Assets/Internal/ClientScript.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using CreativeSpore.RpgMapEditor;
 
 public class ClientScript : MonoBehaviour {
 
@@ -8,14 +9,20 @@ public class ClientScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		Instantiate(autoTileMap);
+		Debug.LogFormat("Start");
+
+		var map = (AutoTileMap)autoTileMap.GetComponent("AutoTileMap");
+		var filePath = "Assets/Map.xml";
+		AutoTileMapSerializeData mapData = AutoTileMapSerializeData.LoadFromFile(filePath);
+		map.MapData.Data.CopyData(mapData);
+		map.LoadMap();
+
 		//var playerPrefab = (GameObject)Resources.Load("/Assets/CreativeSpore/RpgMapEditor/Samples/Prefabs/Player.prefab", typeof(GameObject));
 		Instantiate(playerPrefab, new Vector3(25.0f,-25.0f), Quaternion.identity);
-		
-		Debug.LogFormat("Hello world");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	}
+
 }
