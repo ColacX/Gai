882d310aa4a103aa64c4b73c55eb044756823d23
ColacX <DanielZheng1989@gmail.com>
2016-10-28 17:16:43 +0200

    auto tile map instantiate

--- Gai.Unity/Assets/Internal/ClientScript.cs
+++ Gai.Unity/Assets/Internal/ClientScript.cs
@@ -4,17 +4,18 @@ using System.Collections;
 public class ClientScript : MonoBehaviour {
 
 	public GameObject playerPrefab;
+	public GameObject autoTileMap;
 
 	// Use this for initialization
 	void Start () {
+		Instantiate(autoTileMap);
 		//var playerPrefab = (GameObject)Resources.Load("/Assets/CreativeSpore/RpgMapEditor/Samples/Prefabs/Player.prefab", typeof(GameObject));
-		Instantiate(playerPrefab, new Vector3(31.0f,-25.5f), Quaternion.identity);
+		Instantiate(playerPrefab, new Vector3(25.0f,-25.0f), Quaternion.identity);
+		
 		Debug.LogFormat("Hello world");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Debug.LogFormat("Hello world");
-
 	}
 }
